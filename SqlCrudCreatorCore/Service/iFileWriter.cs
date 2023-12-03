4b15984d08a4ccb7128de61f3d1dc9bd1b8e922e
ddavis1418 <ddavis1418@gmail.com>
2023-12-02 23:17:50 -0500

    Created and implemented FileWriter Service

---
+++ SqlCrudCreatorCore/Service/iFileWriter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlCrudCreatorCore.Service
+{
+     public interface iFileWriter
+    {
+          public void WriteToText(string text, string tableName, string directory, bool buildClass = false);
+    }
+}
