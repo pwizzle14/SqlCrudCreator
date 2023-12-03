442a7e304e449b42db547a6fd1f52cfeb0063ac4
ddavis1418 <ddavis1418@gmail.com>
2023-12-02 23:50:34 -0500

    removed Default constructor for Database service

--- SqlCrudCreatorCore/Service/FileWriter.cs
+++ SqlCrudCreatorCore/Service/FileWriter.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SqlCrudCreatorCore.Service
+﻿namespace SqlCrudCreatorCore.Service
 {
     internal class FileWriter : iFileWriter
     {
